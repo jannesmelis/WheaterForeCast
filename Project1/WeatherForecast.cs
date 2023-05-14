c1cc6c535d15c5da4abbce0cb42a035081581e74
Jannes Melis <jannes.melis@live.be>
2023-05-14 13:08:57 +0200

    Renamed property tempatureCel

--- Project1/WeatherForecast.cs
+++ Project1/WeatherForecast.cs
@@ -4,9 +4,9 @@ namespace Project1
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureCel { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureCel / 0.5556);
 
         public string? Summary { get; set; }
     }
